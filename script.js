0707c205f05c4206f0886b040f9daa93aaee42e2
MelindaPasco <66061324+MelindaPasco@users.noreply.github.com>
2020-10-08 12:42:55 +0200

    Challenge 5

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// Challenge 1 - Burger menu
 $('#ch001_menu').click(function() {
     $(this).toggleClass('active');
     $(this).find('div').removeClass('ch001_default');
+});
+
+// Challenge 5 - Notifications
+$('#ch005_search').bind('click', function() {
+    $('#ch005_input').toggleClass('active');
+});
+
+$('#ch005_menu').bind('click', function() {
+    $('#ch005_box').toggleClass('visible');
+    $('#ch005_sidepanel').toggleClass('active');
 });
