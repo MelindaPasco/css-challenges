e3df97e9e509f28aeaee9d510d4b3d4f8d085695
MelindaPasco <66061324+MelindaPasco@users.noreply.github.com>
2020-11-03 15:33:49 +0100

    Challenge 7

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ $('#ch005_menu').bind('click', function() {
     $('#ch005_box').toggleClass('visible');
     $('#ch005_sidepanel').toggleClass('active');
 });
+
+// Challenge 7 - Burger menu
+$('.ch007_user').click(function() {
+    let src = $(this).find("img").attr('src');
+    let name = $(this).find("p").text();
+    $('#ch007_portrait img').attr("src", src);
+    $('#ch007_header').addClass('active');
+    $('#ch007_footer').addClass('active');
+    $('#ch007_footer p').text(name);
+    $('#ch007_portrait').addClass('active');
+});
+$('#ch007_close').click(function() {
+    $('#ch007_header').removeClass('active');
+    $('#ch007_footer').removeClass('active');
+    $('#ch007_portrait').removeClass('active');
+});
